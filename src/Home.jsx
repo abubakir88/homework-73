c2205cfc21ef5c3d59e04758d178758776104871
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-03-28 00:19:05 +0500

    add smth

--- src/Home.jsx
+++ src/Home.jsx
@@ -27,12 +27,21 @@ function Home() {
     fetchItems();
   };
   // DELETE finish //////////////////////////////////
+  // SEARCH start //////////////////////////////////
+  const [searchTerm, setSearchTerm] = useState("");
 
+  // SEARCH finish //////////////////////////////////
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light ">
-      <h1>List of Students</h1>
+      <h1 className="m-4">List of Students</h1>
       <div className="w-75 rounded bg-white border shadow p-4">
         <div className="d-flex justify-content-end">
+          <input
+            type="search"
+            className="form-control w-25 me-2"
+            placeholder="Search"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
           <Link to="/create" className="btn btn-success">
             Add +
           </Link>
@@ -44,45 +53,103 @@ function Home() {
               <th>Firstname</th>
               <th>Lastname</th>
               <th>Group</th>
-              <th>Action</th>
+              <th className="text-end">Action</th>
             </tr>
           </thead>
-          <tbody>
-            {data.map((d, i) => (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td>{d.firstname}</td>
-                <td>{d.lastname}</td>
-                <td>{d.group}</td>
-                <td>
-                  <Link
-                    to={`/read/${d.id}`}
-                    className="btn btn-sm btn-secondary me-2"
-                  >
-                    Read
-                  </Link>
-                  <Link
-                    to={`/update/${d.id}`}
-                    className="btn btn-sm btn-primary me-2"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() =>
-                      confirm(
-                        "Are you sure you want to? , OKni BOSGANDAN SO'NG REFRESH BERSANGIZ ISHLAYDI"
-                      )
-                        ? deleteItem(d.id)
-                        : false
-                    }
-                  >
-                    Delete
-                  </button>{" "}
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {data.length > 0 && (
+            <tbody>
+              {data
+                .filter(
+                  (data) =>
+                    data.firstname
+                      ?.toLowerCase()
+                      .includes(searchTerm.toLowerCase()) ||
+                    data.lastname
+                      ?.toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                )
+
+                .map((data, i) => (
+                  <>
+                    <tr key={i}>
+                      <td>{i + 1}</td>
+                      <td>{data.firstname}</td>
+                      <td>{data.lastname}</td>
+                      <td>{data.group}</td>
+                      <td className="text-end">
+                        <Link
+                          to={`/read/${data.id}`}
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Read
+                        </Link>
+                        <Link
+                          to={`/update/${data.id}`}
+                          className="btn btn-sm btn-primary me-2"
+                        >
+                          Edit
+                        </Link>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() =>
+                            confirm(
+                              "Are you sure you want to? , OKni BOSGANDAN SO'NG REFRESH BERSANGIZ ISHLAYDI"
+                            )
+                              ? deleteItem(data.id)
+                              : false
+                          }
+                        >
+                          Delete
+                        </button>{" "}
+                      </td>
+                    </tr>
+                  </>
+                ))}
+            </tbody>
+          )}
+
+          {/* {data.length > 0 && (
+            <tbody>
+              {data
+                .filter((panel) =>
+                  panel.brand.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((d, i) => (
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td>{d.firstname}</td>
+                    <td>{d.lastname}</td>
+                    <td>{d.group}</td>
+                    <td>
+                      <Link
+                        to={`/read/${d.id}`}
+                        className="btn btn-sm btn-secondary me-2"
+                      >
+                        Read
+                      </Link>
+                      <Link
+                        to={`/update/${d.id}`}
+                        className="btn btn-sm btn-primary me-2"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() =>
+                          confirm(
+                            "Are you sure you want to? , OKni BOSGANDAN SO'NG REFRESH BERSANGIZ ISHLAYDI"
+                          )
+                            ? deleteItem(d.id)
+                            : false
+                        }
+                      >
+                        Delete
+                      </button>{" "}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          )} */}
         </table>
       </div>
     </div>
